68af27ee0e568b2f4168d0ea7e315522270b734f
michaeldeleonrwds-debug <michaeldeleonrwds-debug@users.noreply.github.com>
2026-09-16 23:22:18 +0800

    remove code toggle button

--- script.js
+++ script.js
@@ -2947,11 +2947,6 @@ ${comp.js || ""}
       setTimeout(() => URL.revokeObjectURL(url), 30000);
     };
 
-    modal.querySelector("[data-toggle-code]").onclick = () => {
-      modalBody.classList.toggle("is-code-hidden");
-      requestAnimationFrame(updatePreviewScale);
-    };
-
     modal.querySelectorAll("[data-modal-view]").forEach((btn) => {
       btn.onclick = () => {
         const showCode = btn.dataset.modalView === "code";
